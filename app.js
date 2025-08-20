ee1c2bdd8fd24316c479dfeb891397f79105f836
Leslee Serna <sernalesg@gmail.com>
2025-08-19 19:49:28 -0600

    nombres de amigos

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = ["Juan", "María", "Pedro", "Ana", "Luis", "Sofía", "Carlos", "Lucía", "Javier", "Elena"];
+console.log(amigos);
